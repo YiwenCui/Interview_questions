16096221fc76e40799df9d055c399cd700b69b9d
YiwenCui <cyw1995519@gmail.com>
2020-11-14 23:18:50 -0500

    modified version

--- string_to_int.py
+++ string_to_int.py
@@ -3,16 +3,16 @@
 def add_one(given_array):
     carry = 1
     result = []
-    for i in range(len(given_array)-1,0, -1): 
+    for i in range(len(given_array)-1,-1, -1): 
         total = given_array[i] + carry
         if total == 10:
             carry = 1
         else: carry = 0
-        result[i] = total%10
+        result.insert(0,total%10) 
     if carry ==1:
-        result = result.insert(0,1)
+        result.insert(0,1)
     return result
 
-print(add_one([1,2,3,4]))
+print(add_one([9,9,9]))
 
           
